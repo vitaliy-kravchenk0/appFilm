9b0ebf6c933b6f090954379607c9b73b2669e32f
Vitaliy <vitaliy.blogger@gmail.com>
2020-10-14 17:47:08 +0300

    Выполнил вторую часть практики, продолжаем создавать свое приложение

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-"use strict"
-;
+"use strict";
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
@@ -10,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+}
+if (personalMovieDB.count == 0) {
+    console.log("Вы не просомтрели ни одного фильма :(");
+} else if (personalMovieDB.count < 10) {
+    console.log("Просморено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
